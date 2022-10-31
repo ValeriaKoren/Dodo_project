aceb4835d5549e0f4ad05f292017b0275b87fe53
Валерия Корень <koren_valeriia1@mail.ru>
2022-10-31 12:41:50 +0300

    new project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DodoComponent } from './dodo_pizza/dodo_pizza.component';
+import { DodoService } from './pizza_service/dodo_pizza.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DodoComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [DodoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/dodo_pizza/dodo_pizza.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, OnChanges, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { delay, timer } from 'rxjs';
+import { IPizza } from '../interfaces/ipizza';
+import { DodoService } from '../pizza_service/dodo_pizza.service';
+
+
+@Component({
+  selector: 'app-dodo',
+  templateUrl: './dodo_pizza.component.html',
+  styleUrls: ['./dodo_pizza.component.css'],
+})
+export class DodoComponent implements OnInit{
+
+  constructor(private Dodo: DodoService, private _changeDetectorRef: ChangeDetectorRef){}
+
+  arr: IPizza[] = [];
+  position_counter = 0;
+
+  ngOnInit(): void {
+    this.Dodo.getPizzaList().subscribe(x=>{
+      console.log(x);
+      this.arr = x;
+      this._changeDetectorRef.markForCheck();
+    })
+  }
+
+  addPizza(){
+    this.position_counter++;
+    console.log(this.position_counter)
+  }
+
+}
